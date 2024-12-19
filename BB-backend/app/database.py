bc6737751c87df45816625053d31ffef181013a3
Igor Pylin <garikpiterson@gmail.com>
2024-12-19 05:49:56 +0200

    Update CI/CD configuration to use Heroku PostgreSQL

--- BB-backend/app/database.py
+++ BB-backend/app/database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Heroku PostgreSQL использует ssl
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
